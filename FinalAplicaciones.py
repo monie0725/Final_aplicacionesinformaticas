4a184608a59ba46ad26c8fbc55fc7b37234ddb8b
monie0725 <99441621+monie0725@users.noreply.github.com>
2023-06-01 17:20:32 -0500

    Cambio2
    Se le agregó el cambio 2 al archivo anterior que contenía el codigo base y el cambio 1

--- FinalAplicaciones.py
+++ FinalAplicaciones.py
@@ -44,3 +44,11 @@ def refresh_board(self):
         for widget in self.winfo_children():
             widget.destroy()
         self.create_board()
+
+        def move_piece(self, row, col):
+        selected_row, selected_col = self.selected_piece
+        piece = self.board[selected_row][selected_col]
+        self.board[selected_row][selected_col] = ' '
+        self.board[row][col] = piece
+        self.selected_piece = None
+        self.refresh_board()
